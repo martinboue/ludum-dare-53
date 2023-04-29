207691c69c218fa681d239204797d321c3f3f187
thewrath <thomaslegoff56@laposte.net>
2023-04-29 21:28:05 +0200

    fix: use parent as car to control instead of explicit exported var

--- src/nodes/controls/car_controller.gd
+++ src/nodes/controls/car_controller.gd
@@ -1,12 +1,10 @@
 extends Node
 
-@export var car : Car
-
 func _physics_process(_delta):
 	drive_car()
 
 func drive_car():
-	if not car is Car:
-		return
-	car.acceleration = Input.get_axis("brake", "accelerate")
-	car.turn = Input.get_axis("steer_left", "steer_right") * deg_to_rad(car.max_steering_angle)
+	var car_to_pilot = get_parent()
+	if car_to_pilot is Car:
+		car_to_pilot.acceleration = Input.get_axis("brake", "accelerate")
+		car_to_pilot.turn = Input.get_axis("steer_left", "steer_right") * deg_to_rad(car_to_pilot.max_steering_angle)
