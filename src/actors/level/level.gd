3af31fc49728fa07b526e9be848e48bb13cbb0f7
Martin Boué <martbou35@gmail.com>
2023-04-30 01:33:37 +0200

    Create tilemap + install smart shape 2d

--- src/actors/level/level.gd
+++ src/actors/level/level.gd
@@ -3,8 +3,3 @@ extends Node2D
 signal new_objective(text: String)
 signal won()
 
-@onready var curve = $Path2D.curve
-@onready var line = $Line2D
-
-func _ready():
-	line.points = curve.tessellate()
