5d12fd10af915df1b35e1c15f86b829b20f3ae7a
Martin Boué <martbou35@gmail.com>
2023-04-29 14:15:55 +0200

    Change trailer physics

--- src/actors/trailer/trailer.gd
+++ src/actors/trailer/trailer.gd
@@ -2,6 +2,5 @@ extends RigidBody2D
 
 
 func _ready() -> void:
-	if owner is RigidBody2D:
-		$PinJoint2D.node_a = owner.get_path()
-
+	if get_parent() is RigidBody2D:
+		$PinJoint2D.node_a = get_parent().get_path()
