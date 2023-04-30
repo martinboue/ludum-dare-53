2228a3c350f5fc5a1172c52d4388a8cc13f604fa
thewrath <thomaslegoff56@laposte.net>
2023-04-30 13:44:38 +0200

    feat: enable enemies based on checkpoint

--- src/nodes/controls/tesla_auto_pilot.gd
+++ src/nodes/controls/tesla_auto_pilot.gd
@@ -1,20 +1,24 @@
 extends Node
 
+@export var speed := 1.0
+
 var target: Node2D
+var car_to_pilot: Car
+
+var is_active := false
 
 func _ready():
 	target = get_tree().get_nodes_in_group("player")[0]
+	car_to_pilot = get_parent()
 
-func _physics_process(_delta):
-	var car_to_pilot = get_parent()
-	if car_to_pilot is Car: 
-		car_to_pilot.acceleration = 1
-		car_to_pilot.turn = car_to_pilot.get_angle_to(target.global_position)
-
+func _physics_process(_delta) -> void:
+	if car_to_pilot == null or not is_active:
+		return 
+	car_to_pilot.acceleration = speed
+	car_to_pilot.turn = car_to_pilot.get_angle_to(target.global_position)
 
 func _on_health_died() -> void:
 	set_physics_process(false)
-	var car_to_pilot = get_parent()
-	if car_to_pilot is Car: 
+	if car_to_pilot != null: 
 		car_to_pilot.acceleration = 0.0
 		car_to_pilot.turn = 0.0
