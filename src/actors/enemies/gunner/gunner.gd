5afe03585b111fe1783b9678428d475c5c5e1644
Martin Boué <martbou35@gmail.com>
2023-04-30 23:54:02 +0200

    Shoot multiple bullets

--- src/actors/enemies/gunner/gunner.gd
+++ src/actors/enemies/gunner/gunner.gd
@@ -21,13 +21,14 @@ func _physics_process(_delta: float) -> void:
 	if is_active and is_triggered and can_shoot():
 		velocity = Vector2.ZERO
 		if fire_cooldown.is_stopped():
-			fire()
+			fire_cooldown.start()
+			var tween = create_tween().set_loops(3)
+			tween.tween_callback(fire).set_delay(0.1)
 
 func can_shoot() -> bool:
 	return target.global_position.distance_to(global_position) < shooting_distance
 
 func fire() -> void:
-	fire_cooldown.start()
 	var bullet: Bullet = bullet_scene.instantiate()
 	bullet.source = bullet_spawner
 	bullet.target = target
