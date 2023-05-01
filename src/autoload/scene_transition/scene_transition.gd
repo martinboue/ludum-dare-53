f845691eda0147b08f38070130c7886eabecf754
Martin Boué <martbou35@gmail.com>
2023-05-01 14:11:40 +0200

    Add loadingmessage

--- src/autoload/scene_transition/scene_transition.gd
+++ src/autoload/scene_transition/scene_transition.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 
 @onready var anim_player = $AnimationPlayer
 
+
 func change_scene(scene_path: String) -> void:
 	anim_player.play("fade")
 	await anim_player.animation_finished
