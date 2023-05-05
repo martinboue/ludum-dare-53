8853c2672c20e3788129418dd4873e61d99864a0
Martin Boué <martbou35@gmail.com>
2023-05-05 21:12:05 +0200

    Remove scene loader

--- src/actors/level/level.gd
+++ src/actors/level/level.gd
@@ -1,18 +1,3 @@
 class_name Level
 extends Node2D
 
-signal new_objective(text: String)
-signal finished(victory: bool)
-
-func _ready() -> void:
-	Score.start()
-
-func finish(victory: bool) -> void:
-	Score.stop(victory)
-	finished.emit(victory)
-
-func end() -> void:
-	finish(true)
-
-func _on_player_car_died() -> void:
-	finish(false)
--- src/menus/main_menu/main_menu.gd
+++ src/menus/main_menu/main_menu.gd
@@ -7,7 +7,8 @@ func _ready() -> void:
 	$VBoxContainer/StartButton.grab_focus()
 
 func _on_start_button_pressed() -> void:
-	SceneTransition.change_scene("res://main.tscn")
+	print("Start: ", Time.get_ticks_msec())
+	get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
